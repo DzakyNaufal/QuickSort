89b45b912f4d454b17cbd8eac422b903ac026b08
DzakyNaufal <dzakynmulna@gmail.com>
2022-10-19 16:56:48 +0700

    Menambahkan method input

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -16,6 +16,24 @@ namespace QuickSort
         // Number of elements in array
         private int n;
 
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+        }
+
     }
 
 }
