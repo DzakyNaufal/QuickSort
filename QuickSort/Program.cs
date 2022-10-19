6871ef1cfab19a5e92b5bd1578fa5e650780dc9d
DzakyNaufal <dzakynmulna@gmail.com>
2022-10-19 17:55:12 +0700

    menambahkan fungsi if

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -90,7 +90,19 @@ namespace QuickSort
                     mov_count++;
                 } 
             }
+            // j now contains the index of the last element in the sorted list
 
+            if (low < j)
+            {
+                //Move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low,j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
         }
 
     }
